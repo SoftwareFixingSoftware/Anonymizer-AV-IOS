1b1cc1aa799a15c5eb81743443de8e24637dcd16
luke(mac) <softwarefixingsoftware@gmail.com>
2025-08-18 04:41:57 +0300

    Initial Commit

---
+++ Anonymizer AV/Anonymizer_AVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Anonymizer_AVApp.swift
+//  Anonymizer AV
+//
+//  Created by gitiriku lucas mwangi on 18/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Anonymizer_AVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
